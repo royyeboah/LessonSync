08bd46a2208baa5e3f3038e2edeae0af375f5d2c
royyeboah <nyarkoyeboahroy@gmail.com>
2024-12-30 13:32:43 +0000

    Added attributes for the TimeTable and Lecture classes

--- src/main/java/com/adbdti/lessonsync/Model/Lecture.java
+++ src/main/java/com/adbdti/lessonsync/Model/Lecture.java
@@ -1,17 +1,27 @@
 package com.adbdti.lessonsync.Model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-public class Event {
+@Entity
+public class Lecture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String day;
     private String course;
     private String location;
     private String lecturer_name;
-    private String group;
+    private String groupName;
     private LocalDateTime start_time;
     private LocalDateTime end_time;
+
+    @ManyToOne
+    @JoinColumn(name="time_table_id", nullable=false)
+    private TimeTable time_table;
 }
--- src/main/java/com/adbdti/lessonsync/Model/TimeTable.java
+++ src/main/java/com/adbdti/lessonsync/Model/TimeTable.java
@@ -1,13 +1,23 @@
 package com.adbdti.lessonsync.Model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
+@Entity
 public class TimeTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    @OneToMany(mappedBy = "time_table", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Lecture> lectures;
 }
