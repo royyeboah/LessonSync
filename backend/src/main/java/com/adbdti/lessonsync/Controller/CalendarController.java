397c1436d181e9fad19ab1005c1d6c418d2b89ad
royyeboah <nyarkoyeboahroy@gmail.com>
2025-01-09 12:28:26 +0000

    Added tailwind to the project

--- backend/src/main/java/com/adbdti/lessonsync/Controller/CalendarController.java
+++ backend/src/main/java/com/adbdti/lessonsync/Controller/CalendarController.java
@@ -63,6 +63,8 @@ public class CalendarController {
             );
             eventLinks.add(eventLink);
         }
+
+        timeTableRepository.deleteAll();
         
         return ResponseEntity.ok(eventLinks);
     }
