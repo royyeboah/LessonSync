9f94432827eb639af5801ba6c08c8054dba97197
roy <nyarkoyeboahroy@gmail.com>
2025-01-12 13:27:44 +0000

    Created the form to upload data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {WelcomeComponent} from './components/welcome/welcome.component';
+import {UploadComponent} from './components/upload/upload.component';
 
 export const routes: Routes = [
 
-  {path: '', component: WelcomeComponent}
+  {path: '', component: WelcomeComponent},
+  {path:'upload', component: UploadComponent}
 ];
