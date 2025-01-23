a6f78419c80460c4f21a36492d752c1378ebb176
roy <nyarkoyeboahroy@gmail.com>
2025-01-23 09:14:40 +0000

    Added service code to create calendar and get current timetable

--- frontend/src/app/services/timetable-service.service.ts
+++ frontend/src/app/services/timetable-service.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Timetable} from '../models/timetable.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimetableServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  apiUrl: string = 'http://localhost:8080';
+
+  createCalendar(timetable: Timetable): Observable<any>{
+
+    return this.http.post(this.apiUrl + '/createCalendar', timetable, {
+      responseType: 'text' as 'json'
+    });
+  }
+
+  getCurrentTimetable(): Observable<Timetable>{
+
+    return this.http.get<Timetable>(`${this.apiUrl}/timetable`)
+  }
+
 }
